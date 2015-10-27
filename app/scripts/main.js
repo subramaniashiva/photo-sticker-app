1768721bb72d4668d1ee691f20b4d4a0a3a6ed22
subramaniashiva <vengaishiva@gmail.com>
2015-10-27 23:59:47 +0530

    Updated sticker features

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,7 @@
     }
     return node;
   }
+  PHOTOAPP.photoAdded = false;
   PHOTOAPP.isFileAnImage = function(fileObj) {
     var imageType = false,
         matchString = /^image\//;
@@ -55,9 +56,12 @@
     $stickerTemplate = document.getElementById('sticker-template');
 
     $photoUploadBtn.addEventListener('change', function() {
+      var img;
       files = this.files;
       if(files.length === 1) {
-        PHOTOAPP.addImgFileToDom($canvasArea, files[0], true);
+        img = PHOTOAPP.addImgFileToDom($canvasArea, files[0], true);
+        img.classList.add('main-image');
+        PHOTOAPP.photoAdded = true;
       }
     });
 
@@ -74,25 +78,32 @@
     });
     var dragged;
     document.addEventListener('dragstart', function(event) {
-          console.log('drag start called');
+        if(PHOTOAPP.photoAdded && event.target.classList.contains('sticker-img')) {
           dragged = event.target;
+        }
     });
-    $canvasArea.addEventListener('drop', function(event){
-      event.preventDefault();
+    $canvasArea.addEventListener('drop', function(event) {
+      var img, canvas, context, dataurl, parent;
+      console.log(event);
+      if(dragged && PHOTOAPP.photoAdded) {
+        event.preventDefault();
 
-      var img = document.createElement('img');
+        img = document.createElement('img');
 
-      var canvas = document.createElement("canvas")
-      var context = canvas.getContext("2d")
-      context.drawImage(dragged, 0, 0) // i assume that img.src is your blob url
-      var dataurl = canvas.toDataURL("image/jpg", 1);
-      img.src = dataurl;
-
-      var parent = this;
-      img.onload = function() {
-        parent.appendChild(img);
-      };
-      
+        canvas = document.createElement("canvas")
+        context = canvas.getContext("2d")
+        context.drawImage(dragged, 0, 0);
+        dataurl = canvas.toDataURL("image/png", 1);
+        img.classList.add('dropped-sticker');
+        img.style.left = (event.offsetX - event.toElement.offsetLeft - 30) + "px";
+        img.style.top = (event.offsetY - event.toElement.offsetTop - 30) + "px";
+        img.src = dataurl;
+        parent = this;
+        img.onload = function() {
+          parent.appendChild(img);
+        };
+        dragged = null;
+      }
     });
     $stickerForm.addEventListener('submit', function(e) {
       var temp, title, $tempDiv = document.createElement('div'),
